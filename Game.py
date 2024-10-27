b2fc31121dc7e7a50d4a3c6999f4ad230183763f
sevplay <sachachist69@gmail.com>
2024-10-27 15:15:10 +0500

    Создание окна настроек

--- Game.py
+++ Game.py
@@ -1,17 +1,30 @@
 import tkinter as tk
 
+def Settings():
+    mainWindow.destroy()
+
+    settingsWindow = tk.Tk()
+    settingsWindow.configure(width=1000,
+                         height=500)
+    settingsWindow.resizable(False, False)
+    settingsWindow.title('Settings')
+
+    settingsWindow.mainloop()
+    main()
+
 
 def main():
+    global mainWindow
     mainWindow = tk.Tk()
     mainWindow.configure(width=1000,
-                         height=500,)
+                         height=500)
     mainWindow.resizable(False, False)
     mainWindow.title('Main')
 
     settings = tk.Button(mainWindow,
                          width=1,
                          height=10,
-                         command=main,
+                         command=Settings,
                          )
     settings.place(x=10, y=10)
 
